f08ff81b6090eb8238e2a29a842f47fd8f48a184
Namdak Tonpa <maxim@synrc.com>
2017-02-26 21:17:47 +0200

    cleanup

--- src/server/fix.erl
+++ src/server/fix.erl
@@ -2,24 +2,12 @@
 -behaviour(application).
 -include_lib("fix_macros.hrl").
 -compile(export_all).
--export([ start/2, stop/1, start_phase/3, prep_stop/1, config_change/3 ]).
+-export([ start/2, stop/1 ]).
 
-test() -> eunit:test(fix).
-gen() -> fix_gen:generate("priv/FIX42.xml").
-
+test()      -> eunit:test(fix).
+gen()       -> fix_gen:generate("priv/FIX42.xml").
+stop(State) -> ok.
 start(Type, StartArgs) ->
-    ?DBG("starting fix  type=[~w], args=[~w]", [Type, StartArgs]),
     case fix_supervisor:start(StartArgs) of
          {ok, Pid} -> {ok, Pid};
          Error -> Error end.
-
-stop(State) -> ?DBG("stop fix. state=[~w]~n",[State]), ok.
-
-start_phase(Phase, StartType, PhaseArgs) ->
-    ?DBG("fix:start_phase() ~w, ~w, ~w", [Phase, StartType, PhaseArgs]), ok.
-
-prep_stop(State) ->
-    ?DBG("fix:pre_stop() ~w", [State]), State.
-
-config_change(Changed, New, Removed) ->
-    ?DBG("fix:config_change(Changed=[~w], New=[~w], Removed=[~w])", [Changed, New, Removed]), ok.
